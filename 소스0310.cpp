a51e43b84e453caff48d9e78f05d4af58a5b863c
JRiho <157566420+JRiho@users.noreply.github.com>
2025-03-19 19:55:09 +0900

    Add files via upload

--- 소스0310.cpp
+++ 소스0310.cpp
@@ -902,30 +902,30 @@
 
 #include <stdio.h>
 
-int main()
-{
-	int input1, input2;
-	char oper;
-	printf("숫자를 입력하세요 : ");
-	scanf("%d", &input1);
-	printf("사칙연산을 입력하세요 : ");
-	scanf(" %c", &oper); // 스페이스바가 공백문자를 걸러줌.
-	printf("숫자를 입력하세요 : ");
-	scanf("%d", &input2);
-
-
-	switch (oper)
-	{
-	case '+': printf("%d + %d = %d", input1, input2, input1 + input2); break;
-	case '-': printf("%d - %d = %d", input1, input2, input1 - input2); break;
-	case '*': printf("%d * %d = %d", input1, input2, input1 * input2); break;
-	case '/': printf("%d / %d = %d", input1, input2, input1 / input2); break;
-	case '%': printf("%d %% %d = %d", input1, input2, input1 % input2); break;
-	default: printf("사칙연산을 잘 입력해주세요");
-		break;
-	}
-	return 0;
-}
+//int main()
+//{
+//	int input1, input2;
+//	char oper;
+//	printf("숫자를 입력하세요 : ");
+//	scanf("%d", &input1);
+//	printf("사칙연산을 입력하세요 : ");
+//	scanf(" %c", &oper); // 스페이스바가 공백문자를 걸러줌.
+//	printf("숫자를 입력하세요 : ");
+//	scanf("%d", &input2);
+//
+//
+//	switch (oper)
+//	{
+//	case '+': printf("%d + %d = %d", input1, input2, input1 + input2); break;
+//	case '-': printf("%d - %d = %d", input1, input2, input1 - input2); break;
+//	case '*': printf("%d * %d = %d", input1, input2, input1 * input2); break;
+//	case '/': printf("%d / %d = %d", input1, input2, input1 / input2); break;
+//	case '%': printf("%d %% %d = %d", input1, input2, input1 % input2); break;
+//	default: printf("사칙연산을 잘 입력해주세요");
+//		break;
+//	}
+//	return 0;
+//}
 
 //#include <stdio.h>
 //
@@ -951,5 +951,404 @@ int main()
 //	case 12: printf("December"); break;
 //	default: printf("숫자를 잘못 입력했습니다.");
 //		break;
+//	}
+//}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char command;
+//
+//	printf("<, a 왼쪽 이동 \n");
+//	printf(">, d 오른쪽 이동 \n");
+//	scanf("%c", &command);
+//
+//	switch (command)
+//	{
+//	case '<':
+//	case 'a':
+//		printf("왼쪽으로 이동합니다.\n");
+//		break;
+//	case '>': case 'd':
+//		printf("오른쪽으로 이동합니다.\n");
+//		break;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int number;
+//	const int option = 3;
+//	printf("메뉴를 선택하세요");
+//	printf("1.새 게임 2. 이어하기 3. 옵션 \n");
+//	scanf("%d", &number);
+//
+//	switch (number)
+//	{
+//	case 1:
+//		printf("새 게임 시작.\n");
+//		break;
+//	case 2:
+//		printf("세이브 데이터 로드.\n");
+//		break;
+//	case option:
+//		printf("옵션 세팅\n");
+//		break;
+//	default:
+//		printf("지정된 기능이 없습니다.\n");
+//		break;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int number;
+//	printf("0~9숫자를 입력하세요.\n");
+//	scanf("%d", &number);
+//	switch (number)
+//	{
+//	case 3: case 6: case 9:
+//		printf("짝");
+//		break;
+//	default:
+//		printf("%d", number);
+//		break;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char command = 'x';
+//	const int x = 120;
+//
+//	switch (command)
+//	{
+//	case 'x': printf("알파벳 x 입력.\n"); break;
+//	case 'X': printf(" 엑스표 입력.\n");	 break;
+//
+//	default:
+//		break;
+//	}
+//	return 0;
+//
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i_menu;
+//	printf("1.더하기 2.빼기\n");
+//	scanf("%d", &i_menu);
+//	switch (i_menu)
+//	{
+//	case 1:
+//	{
+//		int number1, number2;
+//
+//		printf("숫자 두개를 입력하세요");
+//		scanf("%d %d", &number1, &number2);
+//		printf("%d + %d = %d\n", number1, number2, number1 + number2);
+//		break;
+//	}
+//	case 2:
+//	{
+//		int number3, number4;
+//		scanf("%d %d", &number3, &number4);
+//		printf("%d - %d = %d\n", number3, number4, number3 - number4);
+//		break;
+//	}
+//	default:
+//		break;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char command;
+//	printf("wasd 중 하나를 입력하세요\n");
+//	scanf("%c", &command);
+//	switch (command)
+//	{
+//	case 'w': case 'W':
+//		printf("위 방향키를 입력하셨습니다.");
+//		break;
+//	case 'a': case 'A':
+//		printf("좌 방향키를 입력하셨습니다.");
+//		break;
+//	case 'd': case 'D':
+//		printf("우 방향키를 입력하셨습니다.");
+//		break;
+//	case 's': case 'S':
+//		printf("아래 방향키를 입력하셨습니다.");
+//		break;
+//	default:
+//		printf("잘못 입력하셨습니다.");
+//		break;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int appetizer, mainDish, dessert;
+//	printf("appetizer 1~3까지 골라주세요\n");
+//	scanf("%d",&appetizer);
+//	printf("mainDish1~3까지 골라주세요\n");
+//	scanf("%d", &mainDish);
+//	printf("dessert 1~3까지 골라주세요\n");
+//	scanf("%d", &dessert);
+//	printf("주문한 음식은 ");
+//	switch (appetizer)
+//	{
+//	case 1: printf("캐비어, "); break;
+//	case 2: printf("샐러드, "); break;
+//	case 3: printf("푸아그라, "); break;
+//	default: printf("잘못 입력했어"); break;
+//	}
+//	switch (mainDish)
+//	{
+//	case 1: printf("스테이크, "); break;
+//	case 2: printf("생선요리, "); break;
+//	case 3: printf("양갈비, "); break;
+//	default: printf("잘못 입력했어"); break;
+//	}
+//	switch (dessert)
+//	{
+//	case 1: printf("케잌"); break;
+//	case 2: printf("아이스크림"); break;
+//	case 3: printf("초콜릿무스"); break;
+//	default: printf("잘못 입력했어"); break;
+//	}
+//	printf("입니다.");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	while (i < 100)
+//	{
+//		printf("%3d(%02x)", i, i);
+//
+//		if (i >= 70 && i < 80) {i++; continue;}
+//
+//		if (i % 10 == 9) printf("\n");
+//		if (i == 93) break;
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	while (0)
+//	{
+//		printf("조건이 참일 경우 반복 출력\n");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	while (1)
+//	{
+//		printf("조건이 참일 경우 반복 출력\n");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int count = 0;
+//	while (count < 3)
+//	{
+//		printf("현재 count : %d\n", count);
+//		printf("count가 3보다 작은 동안 반복\n");
+//		count++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int count = 3;
+//	
+//	while (count > 0)
+//	{
+//		printf("현재 count : %d \n", count);
+//		printf("count가 0보다 큰 동안 반복\n");
+//		count--;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int count = 0;
+//
+//	while (count < 6)
+//	{
+//		printf("c\n");
+//		count++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int count = -10;
+//
+//	while (count <= 0)
+//	{
+//		printf("%d\n", count);
+//		count++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int count = 1;
+//	while (count <= 100)
+//	{
+//		if (count % 5 == 0)
+//		{
+//			printf("%d\n", count);
+//		}
+//		count++;
+//	}
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int count = 3;
+//	while (count)
+//	{
+//		if (count == 1)
+//		{
+//			break;
+//		}
+//		printf("현재 count : %d\n", count);
+//		printf("count가 0이 아니면 반복\n");
+//		count--;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main() {
+//	char alphabet;
+//	while (1)
+//	{
+//		printf("알파벳을 입력해주세요(다른 것 입력시 다시입력):");
+//		scanf("%c", &alphabet);
+//		if (alphabet >= 'a' && alphabet <= 'z')
+//		{
+//			printf("소문자를 입력하셨습니다.\n");
+//		}
+//		else if (alphabet >= 'A' && alphabet <= 'Z')
+//		{
+//			printf("대문자를 입력하셨습니다.\n");
+//		}
+//		else
+//		{
+//			continue;
+//		}
+//		printf("입력한 값 : %c\n", alphabet);
+//	}
+//
+// }
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int number, total = 0;
+//	while (1)
+//	{
+//		printf("양수를 입력해주세요 (0입력시 종료) : ");
+//		scanf("%d", &number);
+//
+//		if (number == 0) { break; }
+//		else if (number < 0) { continue; }
+//
+//		total = total + number;
+//		printf("%d\n", total);
+//	}
+//	return 0;
+//}
+
+#include <stdio.h>
+
+int main()
+{
+	int number = 1;
+	while (1)
+	{
+		int multi = number;
+		multi *= multi;
+
+		if (multi >= 50000)
+		{
+			printf("곱셈 결과는 %d", multi);
+			printf("숫자는 %d", number);
+			break;
+		}
+		number++;
+	}
+}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	/*char alphabet;
+//	alphabet = 65;
+//
+//	printf("%c", alphabet);*/
+//
+//	char alphabet ='A';
+//	while (alphabet <= 'Z')
+//	{
+//		printf("%c", alphabet);
+//		if (alphabet == 'F') { continue; }
+//		alphabet++;
 //	}
 //}
