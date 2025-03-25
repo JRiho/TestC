4ff1810af258c76a537f9682c0fef4520e30006e
JRiho <157566420+JRiho@users.noreply.github.com>
2025-03-25 22:45:01 +0900

    Add files via upload

---
+++ 소스0326.cpp
@@ -0,0 +1,70 @@
+//194p 6 - 3, 205p 6 - 13, 208p 6 - 14, 210p 6 - 15 타이핑해오기
+
+
+//#include <stdio.h>
+//
+//void main()
+//{
+//	int i;
+//	for (i = 0;i < 3;i++)
+//	{
+//		printf("안녕하세요\n");
+//		printf("##또 안녕하세요?## \n");
+//	}
+//
+//	printf("\n\n");
+//
+//	for (i = 0;i < 3;i++)
+//		printf("안녕하세요?\n");
+//		printf("##또 안녕하세요?##\n");
+//}
+
+
+//#include <stdio.h>
+//
+//void main()
+//{
+//	int i, k;
+//
+//	for (i = 0;i < 3;i++)
+//	{
+//		for (k = 0;k < 2;k++)
+//		{
+//			printf("중첩 for문입니다.(i값 : %d, k값 :%d)\n", i, k);
+//		}
+//	}
+//}
+
+
+//#include <stdio.h>
+//
+//void main()
+//{
+//	int i, k;
+//
+//	for (i = 2; i <= 9; i++)
+//	{
+//		for (k = 1; k <= 9; k++)
+//		{
+//			printf("%d X %d = %d\n", i, k, i * k);
+//		}
+//		printf("\n");
+//	}
+//}
+
+#include <stdio.h>
+
+void main()
+{
+	int i, k;
+
+	for (i = 1; i <= 9; i++)
+	{
+		for (k = 2; k <= 9; k++)
+		{
+			printf("%2dX%2d=%2d",k, i, k * i);
+		}
+		printf("\n");
+	}
+}
+
